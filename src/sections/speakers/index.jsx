08f82655dcad16713b0868369b05d44ac0aba07b
Ishan Sharma <ishandeveloper@outlook.com>
2020-11-15 16:30:13 +0530

    Added Responsive Sidebar

--- src/sections/speakers/index.jsx
+++ src/sections/speakers/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Speakers = () => {
-  return <section>Speakers</section>;
+  return <section></section>;
 };
 
 export default Speakers;
--- src/sections/sponsors/index.jsx
+++ src/sections/sponsors/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Sponsors = () => {
-  return <section>Sponsors</section>;
+  return <section></section>;
 };
 
 export default Sponsors;
