9e1894e1e0b520f75954dcf7dc8007125605f5d9
Ishan Sharma <ishandeveloper@outlook.com>
2020-11-23 03:25:09 +0530

    Landing

--- src/sections/landing/index.jsx
+++ src/sections/landing/index.jsx
@@ -4,76 +4,87 @@ import Lottie from "lottie-react-web";
 import animation from "./lottie.json";
 import Popup from "./Popup";
 
-
-class Landing extends Component{
-  
-  constructor(props){
+class Landing extends Component {
+  constructor(props) {
     super(props);
     this.state = { showPopup: false };
-    }
-  
-    togglePopup() {
-     this.setState({
-       showPopup: !this.state.showPopup
-     });
-}
-  
-  render(){
-  return (
-    <section className="row landing__wrapper">
-      <div className="col-md-6 landing__content">
-        <video className="landing__logo" autoPlay={true} loop={true} muted={true} playsInline={true}>
-          <source src="images/logos/dsc-wow-white.webm" type="video/webm" />
-          {/* <source src="my-animation.mp4" type="video/mp4" /> */}
-        </video>
+  }
 
-        <span className="landing__date">
-          <i className="fas fa-calendar-alt"></i> December 7-13, 2020
-          <span className="landing__separator">|</span>
-          <i className="fas fa-globe"></i> Online
-        </span>
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup,
+    });
+  }
 
-        <div className="landing__description">
-          DSC <span style={{ fontWeight: "500" }}>Week Of Wonders</span>, aka
-          DSC WOW. A weeklong event consisting of a{" "}
-          <span className="landing__highlight">
-            {" "}
-            4 days of workshops/sessions{" "}
-          </span>
-          and a 48 hours <span className="landing__highlight"> hackathon </span>
-          named{" "}
-          <span
-            className="landing__highlight"
-            style={{ fontWeight: "600", color: "#db4437" }}
+  render() {
+    return (
+      <section className="row landing__wrapper" id="landing-section">
+        <div className="col-md-6 landing__content">
+          <video
+            className="landing__logo"
+            autoPlay={true}
+            loop={true}
+            muted={true}
+            playsInline={true}
           >
-            CodeOffDuty
+            <source src="images/logos/dsc-wow-white.webm" type="video/webm" />
+            {/* <source src="my-animation.mp4" type="video/mp4" /> */}
+          </video>
+
+          <span className="landing__date">
+            <i className="fas fa-calendar-alt"></i> December 7-13, 2020
+            <span className="landing__separator">|</span>
+            <i className="fas fa-globe"></i> Online
           </span>
-          . But to us developers and tech enthusiasts, this is much more than
-          some workshops and hackathon, it is a place where imagination meets
-          the technology of tomorrow.
-        </div>
 
-        <div className="landing__actions">
-          <button className="secondary__btn">Get Your WOW Badge</button>
+          <div className="landing__description">
+            DSC <span style={{ fontWeight: "500" }}>Week Of Wonders</span>, aka
+            DSC WOW. A weeklong event consisting of a{" "}
+            <span className="landing__highlight">
+              {" "}
+              4 days of workshops/sessions{" "}
+            </span>
+            and a 48 hours{" "}
+            <span className="landing__highlight"> hackathon </span>
+            named{" "}
+            <span
+              className="landing__highlight"
+              style={{ fontWeight: "600", color: "#db4437" }}
+            >
+              CodeOffDuty
+            </span>
+            . But to us developers and tech enthusiasts, this is much more than
+            some workshops and hackathon, it is a place where imagination meets
+            the technology of tomorrow.
+          </div>
 
-          <button className="primary__btn" onClick={this.togglePopup.bind(this)} >Register</button>
-          {this.state.showPopup ?
-         <Popup
-          closePopup={this.togglePopup.bind(this)}
-         />
-         : null
-       }
+          <div className="landing__actions">
+            <a href="https://badge.dscwow.tech" target="_blank" rel="noopener">
+              {" "}
+              <button className="secondary__btn">Get Your WOW Badge</button>
+            </a>
+
+            <button
+              className="primary__btn"
+              onClick={this.togglePopup.bind(this)}
+            >
+              Register
+            </button>
+            {this.state.showPopup ? (
+              <Popup closePopup={this.togglePopup.bind(this)} />
+            ) : null}
+          </div>
         </div>
-      </div>
-      <div className="col-md-6 landing__lottie">
-        <Lottie
-          options={{
-            animationData: animation,
-          }}
-        />
-      </div>
-    </section>
-  )};
-};
+        <div className="col-md-6 landing__lottie">
+          <Lottie
+            options={{
+              animationData: animation,
+            }}
+          />
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Landing;
