94e23b0fc467551532bff8c2473954da7fd41cd9
Ishan Sharma <ishandeveloper@outlook.com>
2020-11-23 03:24:24 +0530

    About

--- src/sections/about/index.jsx
+++ src/sections/about/index.jsx
@@ -5,7 +5,7 @@ import Typed from "react-typed";
 
 const About = () => {
   return (
-    <section className="about__section">
+    <section className="about__section" id="about-section">
       <div className="row ">
         <div className="col-md-5">
           <img
@@ -24,8 +24,8 @@ const About = () => {
               className="about-typer"
               style={{ wordSpacing: "3px", letterSpacing: "1px" }}
               strings={[
-                "<red>11 Speakers<red/>",
                 "<green>121+ DSCs</green>",
+                "<red>11 Speakers<red/>",
                 "<blue>7 Days</blue>",
               ]}
               typeSpeed={30}
@@ -37,15 +37,18 @@ const About = () => {
           </div>
           <div className="about-text">
             <p className="mb-3">
-              <span className="font-weight-bold">Developer Student Clubs</span>{" "}
-              are university based community groups for students interested in
-              Google developer technologies. Students from all undergraduate or
-              graduate programs with an interest in growing as a developer are
-              welcome.
+              <span className="font-weight-bold">DSC WOW </span> is the
+              collaborative event of 100+ DSCs, who have gathered from all over
+              India to bring together students, developers and communities under
+              the same roof, where they can attain knowledge from highly
+              experienced industry professionals and can also polish their
+              coding skills, or maybe just get started with their programming
+              journey.
             </p>
-            <p>
-            By joining a DSC, students grow their knowledge in a <span className="about-highlight">peer-to-peer learning environment</span> and <span className="text-danger"> build solutions </span>for local businesses and their community.
-            </p>
+
+            <a href="#" target="_blank" rel="noopener" disabled>
+              <button className="secondary__btn" disabled>View Collaborating DSCs</button>
+            </a>
           </div>
         </div>
       </div>
