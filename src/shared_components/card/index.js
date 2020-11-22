f1162e93deb58eb240e7ae1bf15e5da395d6fec5
Ishan Sharma <ishandeveloper@outlook.com>
2020-11-23 03:23:16 +0530

    Hackathon Card

--- src/shared_components/card/index.js
+++ src/shared_components/card/index.js
@@ -1,16 +1,37 @@
 import React from "react";
 import "./styles/index.css";
-import Tilt from 'react-tilt';
+import Tilt from "react-tilt";
+import Lottie from "lottie-react-web";
+import animation from "./lottie.json";
+
 const Card = (props) => {
-  let classNames = "card__content Tilt ";
-  classNames+=props.size;
+  // let classNames = "card__content Tilt ";
+  // classNames += props.size;
   return (
-    <Tilt className={classNames} options={{ max : 4 }} >
+    <Tilt
+      className="col-md-9 card__content Tilt"
+      options={{ max: 5, scale: 1 }}
+    >
       <div
         className="card__content__inner Tilt-inner"
-        style={{backgroundColor:props.color,scale:1}}
+        style={{ backgroundColor: props.color, scale: 1 }}
       >
-        <h1>{props.title}</h1>
+        <div className="row">
+          <div className="col-md-6 align-items-start">
+            <div className="hackathon__title">Code Off Duty</div>
+            <div className="hackathon__duration">48 Hours Hackathon</div>
+
+            <a
+              href="https://hackathon.dscwow.tech"
+              target="_blank"
+              rel="noopener"
+            >
+              <button class="buttonfx slideleft">Register Now</button>
+            </a>
+          </div>
+          {/* <div className="col-md-6 hackathon__lottie"> */}
+          <img src="images/card/graphics.png" className="card__graphics" />
+        </div>
       </div>
     </Tilt>
   );
