6f37e17b48c2953169b6dfbe3076e91118d5b4cd
Ishan Sharma <ishandeveloper@outlook.com>
2020-11-25 16:02:07 +0530

    Performance Optimizations

--- src/sections/speakers/index.jsx
+++ src/sections/speakers/index.jsx
@@ -2,6 +2,63 @@ import React from "react";
 import SpeakerCard from "./components/speakercard";
 import "./styles/index.css";
 
+const speakersData = [
+  {
+    name: "Sayak Paul",
+    title: "ML Expert",
+    description:
+      "Deep Learning Associate at PyImageSearch | Co-author of Web-DL book (bit.ly/webdl-book)",
+    linkedin: "https://www.linkedin.com/in/sayak-paul/",
+    image: "images/speakers/sayak.jpg",
+  },
+  {
+    name: "Mrinal Jain",
+    title: "Frontend Engineer",
+    description:
+      "Tech Speaker | Community Lead - Facebook Developer Circles | Frontend Engineer -WittyFeed | Tech Evangelist- Mozilla",
+    image: "images/speakers/mrinal.jpg",
+    linkedin: "https://www.linkedin.com/in/mrinaljain/",
+  },
+  {
+    name: "Sanskar Tiwari",
+    image: "images/speakers/sanskar.jpg",
+    description: "SDE @ PuStack, Educator at Youtube Blog @ flutternerd.com",
+    linkedin: "https://www.linkedin.com/in/lamsanskar/",
+    twitter: "https://twitter.com/Theindianappguy",
+    title: "Flutter Developer",
+  },
+  {
+    name: "Koren Grinshpoon",
+    title: "echoAR",
+    description: "Founder & COO at echoAR (Techstars '19)",
+    image: "images/speakers/koren.jpg",
+    linkedin: "https://www.linkedin.com/in/korengrinshpoon/",
+  },
+  {
+    name: "Nikhil Madhusudan",
+    title: "Program Manager",
+    image: "images/speakers/nikhil.jpg",
+    description: "Program Manager at Google, Former CEO @JellyCone",
+    linkedin: "https://www.linkedin.com/in/nikhilraichur/",
+  },
+  {
+    title: "Developer Advocate",
+    name: "Ada Rose Cannon",
+    linkedin: "https://uk.linkedin.com/in/adarose",
+    description:
+      "Developer Advocate/Staff Engineer at Samsung & Co-chair of the W3C Immersive Group",
+    image: "images/speakers/ada.jpg",
+  },
+  {
+    title: "Program Manager",
+    linkedin: "https://www.linkedin.com/in/sidagarwal04",
+    name: "Siddhant Aggarwal",
+    description:
+      "ACM's Distinguished Speaker | Developer Relations @ Google India | Tech Communities + Edu + Design + Startups",
+    image: "images/speakers/sid.jpg",
+  },
+];
+
 const Speakers = () => {
   return (
     <section className="speakers__wrapper" id="speakers-section">
@@ -12,57 +69,16 @@ const Speakers = () => {
       <div className="row justify-content-center">
         <div className="col-md-9 col-xs-12 ">
           <div className="row">
-            <SpeakerCard
-              name="Sayak Paul"
-              title="ML Expert"
-              description="Deep Learning Associate at PyImageSearch | Co-author of Web-DL
-                book (bit.ly/webdl-book)"
-              linkedin="https://www.linkedin.com/in/sayak-paul/"
-              image="images/speakers/sayak.jpg"
-            />
-            <SpeakerCard
-              name="Mrinal Jain"
-              title="Frontend Engineer"
-              description="Tech Speaker | Community Lead - Facebook Developer Circles | Frontend Engineer -WittyFeed | Tech Evangelist- Mozilla"
-              image="images/speakers/mrinal.jpg"
-              linkedin="https://www.linkedin.com/in/mrinaljain/"
-            />
-            <SpeakerCard
-              name="Sanskar Tiwari"
-              image="images/speakers/sanskar.jpg"
-              description="SDE @ PuStack, Educator at Youtube Blog @ flutternerd.com"
-              linkedin="https://www.linkedin.com/in/lamsanskar/"
-              twitter="https://twitter.com/Theindianappguy"
-              title="Flutter Developer"
-            />
-            <SpeakerCard
-              name="Koren Grinshpoon"
-              title="echoAR"
-              description="Founder & COO at echoAR (Techstars '19)"
-              image="images/speakers/koren.jpg"
-              linkedin="https://www.linkedin.com/in/korengrinshpoon/"
-            />
-            <SpeakerCard
-              name="Nikhil Madhusudan"
-              title="Program Manager"
-              image="images/speakers/nikhil.jpg"
-              description="Program Manager at Google, Former CEO @JellyCone"
-              linkedin="https://www.linkedin.com/in/nikhilraichur/"
-            />
-            <SpeakerCard
-              title="Developer Advocate"
-              name="Ada Rose Cannon"
-              description="Developer Advocate/Staff Engineer at Samsung & Co-chair of the W3C Immersive Group"
-              image="images/speakers/ada.jpg"
-            />
-
-            <SpeakerCard
-              title="Program Manager"
-              linkedin="https://www.linkedin.com/in/sidagarwal04"
-              name="Siddhant Aggarwal"
-              description="ACM's Distinguished Speaker | Developer Relations @ Google India | Tech Communities + Edu + Design + Startups"
-              image="images/speakers/sid.jpg"
-            />
+            {speakersData.map((e) => (
+              <SpeakerCard
+                key={e.linkedin}
+                title={e.title}
+                linkedin={e.linkedin}
+                name={e.name}
+                description={e.description}
+                image={e.image}
+              />
+            ))}
           </div>
         </div>
       </div>
