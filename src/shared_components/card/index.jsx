228a3b77ea2bea3cfdf2e91463733c1a78458589
Mahesh Nahak <56269416+maheshn22@users.noreply.github.com>
2020-11-27 12:25:24 +0530

    Hackathon Card Enabled

--- src/shared_components/card/index.jsx
+++ src/shared_components/card/index.jsx
@@ -21,8 +21,8 @@ const Card = (props) => {
             <div className="hackathon__title">Code Off Duty</div>
             <div className="hackathon__duration">48 Hours Hackathon</div>
 
-            <a  href="#" target="_blank" rel="noopener" aria-disabled>
-              <button class="buttonfx slideleft" disabled>Coming Soon!</button>
+            <a  href="https://hackathon.dscwow.tech/" target="_blank" rel="noopener" >
+              <button class="buttonfx slideleft">Register Now</button>
             </a>
           </div>
           {/* <div className="col-md-6 hackathon__lottie"> */}
