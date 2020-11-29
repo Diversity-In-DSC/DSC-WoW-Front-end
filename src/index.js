ed3eff6f5f132cee4816851b6b9caadbf2641277
Ishan Sharma <ishandeveloper@outlook.com>
2020-11-29 12:44:38 +0530

    Added Preloader

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./main.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 // import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
@@ -15,7 +15,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals(console.log);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
