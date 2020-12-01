ce6dde5469bbdcea98d4f29fd6f66939d61e0995
Yashank18 <46098062+Yashank18@users.noreply.github.com>
2020-12-01 18:40:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./main.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals"
 // import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
